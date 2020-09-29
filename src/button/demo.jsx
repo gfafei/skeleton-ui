3b1ec347689fd08790e45ca4b4f1a64ffbd7a23d
afei <afei_gf@foxmail.com>
2020-09-29 18:06:45 +0800

    demo

--- src/button/demo.jsx
+++ src/button/demo.jsx
@@ -15,6 +15,15 @@ const ButtonDemo = () => {
           <input type="checkbox" checked={state.disabled}
                  onChange={e => setState({ ...state, disabled: e.target.checked })}/>
         </div>
+        <div className="demo-setting-row">
+          <label>label</label>
+          <input value={state.label} onChange={e => setState({ ...state, label: e.target.value })}/>
+        </div>
+        <div className="demo-setting-row">
+          <label>fullwidth</label>
+          <input type="checkbox" checked={state.fullwidth}
+                 onChange={e => setState({ ...state, fullwidth: e.target.checked })}/>
+        </div>
       </div>
       <div className="demo-content">
         <Button
--- src/button/index.jsx
+++ src/button/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import clsx from 'clsx'
-import * as _ from 'lodash';
+import * as _ from 'lodash'
+import './button.less'
 
 const Button = (props) => {
   const {
@@ -30,7 +31,7 @@ Button.propTypes = {
   onClick: propTypes.func,
   disabled: propTypes.bool,
   fullwidth: propTypes.bool,
-  variant: propTypes.oneOf(['contained', 'text'])
+  variant: propTypes.oneOf(['contained', 'text', 'outlined'])
 }
 Button.defaultProps = {
   onClick: _.noop,
